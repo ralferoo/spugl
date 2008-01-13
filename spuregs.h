5d21af38623c5bb648693365e91366865de34ce3
ralferoo <ralferoo@2313fe68-062f-0410-ac29-e3614736eab5>
2008-01-13 10:05:13 +0000

    added explanatory comment
    git-svn-id: https://python-ps3.svn.sourceforge.net/svnroot/python-ps3/trunk/3d@768 2313fe68-062f-0410-ac29-e3614736eab5

--- spuregs.h
+++ spuregs.h
@@ -9,6 +9,14 @@
 // this header file is simply to reserve registers globally for specific
 // variables, although we go to quite a lot of effort to ensure that
 // "make depend" verifies that all SPU sources do actually use this file
+//
+// note that on the SPU, registers 80-127 are non-volatile so anyone who
+// corrupts them must restore them on exit. all our code knows about these
+// registers, so they will not generally be used, but library calls that
+// use them will at least preserve them for us.
+// note also that if we call a library function that calls us in return,
+// these registers might have been corrupted, however, I don't beleive we
+// do this anywhere.
 
 #include "struct.h"
 
@@ -16,7 +24,7 @@
 #define __spuregs_h
 
 #ifdef SPU_REGS
-register u32 ___foo asm ("82");
+register u32 ___foo asm ("80");
 #endif
 
 #endif // __spuregs_h
