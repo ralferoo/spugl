ab5f158db203860ab52fab87a5193220d257a5ed
Ranulf Doswell <dev@ranulf.net>
2008-02-23 17:24:09 +0000

    Starting to rework the texture stuff

--- shader.c
+++ shader.c
@@ -220,8 +220,6 @@ PROCESS_BLOCK_END
 
 //////////////////////////////////////////////////////////////////////////////
 
-extern void* textureCache;
-
 static const vec_uchar16 shuf_gath_01 = {
 	2,3,128,128, 18,19,128,128, SEL_00 SEL_00};
 
@@ -262,14 +260,193 @@ static const vec_uchar16 splats[] = {
 	/* 1110 */ {18,19, 22,23,26,27, 0,1, 2,3,4,5, 6,7,8,9},
 	/* 1111 */ {18,19, 22,23,26,27,30,31, 0,1, 2,3,4,5, 6,7},
 };
+	
+void* loadMissingTextures(void* self, Block* block, int tag,
+			vec_float4 A, vec_uint4 left, vec_uint4* ptr,
+			vec_uint4 block_id, vec_uint4 cache_not_found)
+{
+	block->A = A;
+	block->left = spu_extract(left,0);
+	block->pixels = ptr;
+				
+	block->triangle->count--;
+	return 0;
+}
+
+#define NUMBER_TEX_MAPS 16
+
+u32 textureCache[NUMBER_TEX_MAPS*32*32] __attribute__((aligned(128)));
+
+void* textureMapFill(void* self, Block* block, int tag)
+{
+	Triangle* tri = block->triangle;
+
+	vec_float4 A_dx = tri->A_dx;
+	vec_float4 Aa_dx = spu_splats(spu_extract(A_dx,0));
+	vec_float4 Ab_dx = spu_splats(spu_extract(A_dx,1));
+	vec_float4 Ac_dx = spu_splats(spu_extract(A_dx,2));
+
+	vec_float4 A_dy = block->A_dy;
+	vec_float4 Aa_dy = spu_splats(spu_extract(A_dy,0));
+	vec_float4 Ab_dy = spu_splats(spu_extract(A_dy,1));
+	vec_float4 Ac_dy = spu_splats(spu_extract(A_dy,2));
+
+	vec_float4 A_dx4 = block->A_dx4;
+	vec_float4 Aa_dx4 = spu_splats(spu_extract(A_dx4,0));
+	vec_float4 Ab_dx4 = spu_splats(spu_extract(A_dx4,1));
+	vec_float4 Ac_dx4 = spu_splats(spu_extract(A_dx4,2));
+
+	vec_float4 A = block->A;
+	vec_float4 Aa = spu_madd(muls,Aa_dx,spu_splats(spu_extract(A,0)));
+	vec_float4 Ab = spu_madd(muls,Ab_dx,spu_splats(spu_extract(A,1)));
+	vec_float4 Ac = spu_madd(muls,Ac_dx,spu_splats(spu_extract(A,2)));
+
+	vec_uint4 left = spu_splats(block->left);
+	vec_uint4* ptr = block->pixels;
+	vec_uint4 tex_id_base = spu_splats((unsigned int)tri->tex_id_base);
+
+	do {
+		vec_uint4 uAa = (vec_uint4) Aa;
+		vec_uint4 uAb = (vec_uint4) Ab;
+		vec_uint4 uAc = (vec_uint4) Ac;
+		vec_uint4 allNeg = spu_and(spu_and(uAa,uAb),uAc);
+		vec_uint4 pixel = spu_rlmaska(allNeg,-31);
+		vec_uint4 bail = spu_orx(pixel);
+		if (spu_extract(bail,0)) {
+			vec_float4 t_w = extract(tri->w, Aa, Ab, Ac);
+			vec_float4 w = spu_splats(1.0f)/t_w;
+			vec_float4 tAa = spu_mul(Aa,w);
+			vec_float4 tAb = spu_mul(Ab,w);
+			vec_float4 tAc = spu_mul(Ac,w);
+
+//PROCESS_BLOCK_HEAD(process_tex_block)
+////////////////////
+			vec_float4 t_s = extract(tri->s, tAa, tAb, tAc);
+			vec_float4 t_t = extract(tri->t, tAa, tAb, tAc);
+
+			vec_uint4 s_sub = spu_and(spu_rlmask(spu_convtu(t_s,32),-17), 0xf80);	//19-2
+			vec_uint4 t_sub = spu_and(spu_rlmask(spu_convtu(t_t,32),-22), 0x7c);	//24-2
+			vec_uint4 sub_block_pixel = spu_or(s_sub,t_sub);
+
+			vec_uint4 s_blk = spu_and(spu_rlmask(spu_convtu(t_s,32),-26), 0x38);	//24+2
+			vec_uint4 t_blk = spu_and(spu_rlmask(spu_convtu(t_t,32),-29), 0x7);	//24+5
+			vec_uint4 block_id = spu_add(tex_id_base,spu_or(s_blk,t_blk));
+
+			vec_uchar16 shuf_cmp_0 = spu_splats((unsigned short)0x203);
+			vec_ushort8 copy_cmp_0 = spu_shuffle(block_id,block_id,shuf_cmp_0);
+			vec_ushort8 matches1_0 = spu_cmpeq(TEXcache1,copy_cmp_0);
+			vec_ushort8 matches2_0 = spu_cmpeq(TEXcache2,copy_cmp_0);
+			vec_uint4 gather1_0 = spu_gather((vec_uchar16)matches1_0);
+			vec_uint4 gather2_0 = spu_gather((vec_uchar16)matches2_0);
+
+			vec_uchar16 shuf_cmp_1 = spu_splats((unsigned short)0x607);
+			vec_ushort8 copy_cmp_1 = spu_shuffle(block_id,block_id,shuf_cmp_1);
+			vec_ushort8 matches1_1 = spu_cmpeq(TEXcache1,copy_cmp_1);
+			vec_ushort8 matches2_1 = spu_cmpeq(TEXcache2,copy_cmp_1);
+			vec_uint4 gather1_1 = spu_gather((vec_uchar16)matches1_1);
+			vec_uint4 gather2_1 = spu_gather((vec_uchar16)matches2_1);
+
+			vec_uchar16 shuf_cmp_2 = spu_splats((unsigned short)0xa0b);
+			vec_ushort8 copy_cmp_2 = spu_shuffle(block_id,block_id,shuf_cmp_2);
+			vec_ushort8 matches1_2 = spu_cmpeq(TEXcache1,copy_cmp_2);
+			vec_ushort8 matches2_2 = spu_cmpeq(TEXcache2,copy_cmp_2);
+			vec_uint4 gather1_2 = spu_gather((vec_uchar16)matches1_2);
+			vec_uint4 gather2_2 = spu_gather((vec_uchar16)matches2_2);
+		
+			vec_uchar16 shuf_cmp_3 = spu_splats((unsigned short)0xe0f);
+			vec_ushort8 copy_cmp_3 = spu_shuffle(block_id,block_id,shuf_cmp_3);
+			vec_ushort8 matches1_3 = spu_cmpeq(TEXcache1,copy_cmp_3);
+			vec_ushort8 matches2_3 = spu_cmpeq(TEXcache2,copy_cmp_3);
+			vec_uint4 gather1_3 = spu_gather((vec_uchar16)matches1_3);
+			vec_uint4 gather2_3 = spu_gather((vec_uchar16)matches2_3);
+		
+			vec_uint4 gather_merge=spu_splats((unsigned short)0x5555);
+		
+			vec_uint4 gather_0 = spu_sel(gather1_0, gather2_0, gather_merge);
+			vec_uint4 gather_2 = spu_sel(gather1_2, gather2_2, gather_merge);
+			vec_uint4 gather_1 = spu_sel(gather1_1, gather2_1, gather_merge);
+			vec_uint4 gather_3 = spu_sel(gather1_3, gather2_3, gather_merge);
+		
+			vec_uint4 gather_01 = spu_shuffle(gather_0,gather_1,shuf_gath_01);
+			vec_uint4 gather_23 = spu_shuffle(gather_2,gather_3,shuf_gath_23);
+			vec_uint4 gather = spu_or(gather_01,gather_23);
+			vec_uint4 cache = spu_cntlz(gather);
+			vec_uint4 cache_not_found = spu_cmpeq(cache,spu_splats((unsigned int)32));
+
+			unsigned int cache_orx = spu_extract(spu_orx(cache_not_found),0);
+			if (cache_orx) {  // amazingly gcc does move this out of the loop :)
+				return loadMissingTextures(self, block, tag,
+					A, left, ptr,
+					block_id, cache_not_found);
+			}
+
+			// pixel is mask of 1's where we want to draw
+			// cache_not_found mask of 0's where we have valid data
+			// tex_mask is mask of 1's where we need to draw
+//			pixel = spu_andc(pixel, cache_not_found);
+//			tex_mask = spu_andc(tex_mask, pixel);		// remove pixels we can draw
+		
+			vec_uint4 local_tex_base = spu_splats((unsigned int)&textureCache);
+			vec_uint4 tex_ofs = spu_sl(cache, 5+5+2);	// offset into texture
+			vec_uint4 addr = spu_add(tex_ofs,spu_add(sub_block_pixel,local_tex_base));
+		
+			unsigned long pixel0 = *((u32*)spu_extract(addr,0));
+			unsigned long pixel1 = *((u32*)spu_extract(addr,1));
+			unsigned long pixel2 = *((u32*)spu_extract(addr,2));
+			unsigned long pixel3 = *((u32*)spu_extract(addr,3));
+			vec_uint4 colour = {pixel0, pixel1, pixel2, pixel3};
+		
+			colour = spu_shuffle(colour, colour, rgba_argb);
+		
+			vec_uint4 current = *ptr;
+			*ptr = spu_sel(current, colour, pixel);
+/*		
+			// now try to work out what textures were missing
+			// copy_cmp_[0123] hold required texture ids splatted across all 8 halfwords
+		
+			vec_uint4 check_dups0 = spu_cmpeq(block_id,
+						spu_shuffle(block_id,block_id,dup_check0));
+			vec_uint4 check_dups1 = spu_cmpeq(block_id,
+						spu_shuffle(block_id,block_id,dup_check1));
+			vec_uint4 check_dups2 = spu_cmpeq(block_id,
+						spu_shuffle(block_id,block_id,dup_check2));
+			vec_uint4 is_duplicate = spu_or(spu_or(check_dups0,check_dups1), check_dups2);
+
+			vec_uint4 gotneeds_0 = spu_gather(spu_cmpeq(need1,copy_cmp_0));
+			vec_uint4 gotneeds_1 = spu_gather(spu_cmpeq(need1,copy_cmp_1));
+			vec_uint4 gotneeds_2 = spu_gather(spu_cmpeq(need1,copy_cmp_2));
+			vec_uint4 gotneeds_3 = spu_gather(spu_cmpeq(need1,copy_cmp_3));
+
+			vec_uint4 gotneeds_all = {spu_extract(gotneeds_0,0),spu_extract(gotneeds_1,0),
+				  		spu_extract(gotneeds_2,0), spu_extract(gotneeds_3,0)};
+			vec_uint4 gotneeds_mask = spu_andc(spu_cmpeq(gotneeds_all,0),is_duplicate);
+			
+			vec_uint4 want_textures = spu_and(cache_not_found,gotneeds_mask);
+			
+			vec_uint4 want_gather = spu_gather(want_textures);
+			need1 = spu_shuffle(need1,(vec_ushort8)block_id,
+							splats[spu_extract(want_gather,0)]);
+*/
+//PROCESS_BLOCK_END
+
+		} 
+		vec_uint4 which = spu_and(left,spu_splats((unsigned int)7));
+		vec_uint4 sel = spu_cmpeq(which,1);
+		ptr++;
+		left -= spu_splats(1);
+		 A += spu_sel( A_dx4, A_dy,sel);
+		Aa += spu_sel(Aa_dx4,Aa_dy,sel);
+		Ab += spu_sel(Ab_dx4,Ab_dy,sel);
+		Ac += spu_sel(Ac_dx4,Ac_dy,sel);
+	} while (spu_extract(left,0)>0);
+
+	block->triangle->count--;
+	return 0;
+
+}
 
 /*
 //////////////////////////////////////////////////////////////////////////////
-//
-// This shader does a very ugly form of texture mapping - for each pixel that
-// needs mapping, this fires off a DMA for 128 bytes just to then copy across
-// the 4 bytes of the texture data
-//
 vec_ushort8 process_texture_block(Block* block, 
 		vec_float4 Aa,vec_float4 Ab,vec_float4 Ac, 
 		vec_float4 Aa_dx4,vec_float4 Ab_dx4,vec_float4 Ac_dx4, 
