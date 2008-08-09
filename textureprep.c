babb5863617f962b6557586ae54446d751ae0a30
Ranulf Doswell <dev@ranulf.net>
2008-08-09 22:04:26 +0100

    looks like textures mipmapped correctly but wrong mult on tex load

--- textureprep.c
+++ textureprep.c
@@ -69,19 +69,41 @@ Texture convertGimpTexture(gimp_image* source) {
 		tex->tex_log2_x = log2(width);
 		tex->tex_log2_y = log2(height);
 
+		/*
 		//calculateMipmap(pixels, pixels, pixels, pixels, pixels);
 		for (int a=0; a<width/32*height/32; a++) {
 			void* p = pixels + a*32*32;
 			calculateMipmap(p, p, p, p, p);
-		}
+		}*/
+
 		// fake later mipmaps
 		for (int i=1; i<7; i++) {
-			width = (width/2 + 31)&~31; 	
-			height = (width/2 + 31)&~31; 	
-			tex->tex_id_base[i] = tex->tex_id_base[0];
-			tex->tex_data[i] = tex->tex_data[0];
+			int nwidth = (width/2 + 31)&~31; 	
+			int nheight = (height/2 + 31)&~31; 	
+			mainsize = nwidth*nheight*4;
+
+			buffer = malloc(mainsize+127);
+			u32* pixels_shrink = (u32*) ((((unsigned int)buffer)+127)&~127);
+			printf("new buffer %x-%x\n", pixels_shrink, pixels_shrink+(mainsize-1)/4);
+			for (int x=0; x<nwidth; x+=32) {
+				for (int y=0; y<nheight; y+=32) {
+					u32* d = pixels_shrink + y*32 + x*nheight;
+					u32* s = pixels + y*64 + x*2*height;
+					int dy = height>32 ? 32*32 : 0;
+					int dx = width>32 ? height*32 : 0;
+					printf("calc mipmap %d %d,%d %x %x %d %d (%dx%d->%dx%d)\n", i, x,y, s, d, dx, dy, width, height, nwidth, nheight);
+					calculateMipmap(s, s+dx, s+dy, s+dy+dx, d);
+				}
+			}
+
+			tex->tex_id_base[i] = data[0]; data[0]+=(nwidth*nheight)<<10;
+			tex->tex_data[i] = pixels_shrink;
 			tex->tex_t_mult[i] = tex->tex_t_mult[0];
 			tex->tex_max_mipmap = i;
+
+			pixels = pixels_shrink;
+			width = nwidth;
+			height = nheight;
 		}
 
 	}
