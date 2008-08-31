4039e6f8c7c86cf7481164d5252216d52b2af3d0
Ranulf Doswell <dev@ranulf.net>
2008-08-31 07:07:13 +0100

    added version.sh

--- spugl-client/daemon.h
+++ spugl-client/daemon.h
@@ -11,7 +11,7 @@
 
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 1
-#define VERSION_REVISION 6
+#define VERSION_REVISION 7
 
 struct SPUGL_request {
 	unsigned short command;
---
+++ version.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+TAG=`git tag`
+MAJOR=`echo $TAG|cut -d. -f1`
+MINOR=`echo $TAG|cut -d. -f2`
+REVISION=`git rev-list $TAG..HEAD | wc -l`
+FILE=spugl-client/daemon.h
+
+sed -e "s/VERSION_MAJOR.*$/VERSION_MAJOR $MAJOR/;s/VERSION_MINOR.*$/VERSION_MINOR $MINOR/;s/VERSION_REVISION.*$/VERSION_REVISION $REVISION/;" <$FILE >.version
+mv .version $FILE
+
+echo $MAJOR.$MINOR.$REVISION >version.txt
