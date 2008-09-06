f20ecf0d1a17e83974f526b5c30797937f04df8d
Ranulf Doswell <dev@ranulf.net>
2008-09-06 19:41:35 +0100

    moved to 0.2 branch since the basic buffer management code is now working

--- client/daemon.h
+++ client/daemon.h
@@ -11,8 +11,8 @@
 
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 1
-#define VERSION_REVISION 109
-#define VERSION_STRING "0.1.109"
+#define VERSION_REVISION 112
+#define VERSION_STRING "0.1.112"
 
 typedef struct __SPUGL_request SPUGL_request;
 typedef struct __SPUGL_reply SPUGL_reply;
