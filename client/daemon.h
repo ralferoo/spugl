557e4f295bf9750e3391983c1b307aea6e36ebb5
Ranulf Doswell <dev@ranulf.net>
2008-09-06 19:21:37 +0100

    test

--- client/daemon.h
+++ client/daemon.h
@@ -11,8 +11,8 @@
 
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 1
-#define VERSION_REVISION 82
-#define VERSION_STRING "0.1.82"
+#define VERSION_REVISION 83
+#define VERSION_STRING "0.1.83"
 
 typedef struct __SPUGL_request SPUGL_request;
 typedef struct __SPUGL_reply SPUGL_reply;
