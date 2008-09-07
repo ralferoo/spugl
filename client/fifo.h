1921357b64bb70d05a8f4193ea77b7d55d8cb38f
Ranulf Doswell <dev@ranulf.net>
2008-09-07 22:45:27 +0100

    added client/fifo.h

--- client/fifo.h
+++ client/fifo.h
@@ -12,14 +12,7 @@
 #ifndef __client_fifo_h
 #define __client_fifo_h
 
-typedef void* DriverContext;
-
-#define __SPUMEM_ALIGNED__ __attribute__((aligned(16)))
-#define __CACHE_ALIGNED__ __attribute__((aligned(128)))
-
-#define BYTE_ALIGNMENT 127
-#define PIXEL_ALIGNMENT (BYTE_ALIGNMENT>>2)
-
+#include "queue.h"
 #include "gen_command_defs.h"
 
 //////////////////////////////////////////////////////////////////////////////
@@ -43,8 +36,6 @@ union __f2i
 
 //////////////////////////////////////////////////////////////////////////////
 
-#define SPU_MIN_FIFO 128
-
 inline unsigned int* _begin_fifo(CommandQueue* queue, unsigned int minsize) {
 	unsigned int* ptr = (unsigned int*) ( ((void*)queue) + queue->write_ptr );
 	return ptr;
@@ -55,9 +46,5 @@ inline void _end_fifo(CommandQueue* queue, unsigned int* ptr) {
 	queue->write_ptr = ((void*)ptr) - ((void*)queue);
 	__asm__("lwsync");
 }
-
-struct __TRIANGLE;
-
-typedef void* SPU_COMMAND(void* data, struct __TRIANGLE * triangle);
 
 #endif //  __client_fifo_h
