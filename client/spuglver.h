830f970ec046f18d0008632d8db806ab21733c51
root <root@gimel.home.ranulf.net>
2008-10-11 18:46:23 +0000

    junk

--- client/spuglver.h
+++ client/spuglver.h
@@ -14,7 +14,7 @@
 
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 2
-#define VERSION_REVISION 2
-#define VERSION_STRING "0.2.2"
+#define VERSION_REVISION 3
+#define VERSION_STRING "0.2.3"
 
 #endif // __SPUGL_VER_H
