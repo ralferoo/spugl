c9c231341e22942a5bd10eec4e13e4eb8602b43e
Ranulf Doswell <dev@ranulf.net>
2008-10-20 07:53:24 +0000

    moved textures to test dir

--- client/spuglver.h
+++ client/spuglver.h
@@ -14,7 +14,7 @@
 
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 2
-#define VERSION_REVISION 67
-#define VERSION_STRING "0.2.67"
+#define VERSION_REVISION 68
+#define VERSION_STRING "0.2.68"
 
 #endif // __SPUGL_VER_H
